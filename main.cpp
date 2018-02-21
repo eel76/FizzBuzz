028f917ed21d57df8880f6aeeb5a7b2536d192a2
Dirk Reinbach <dirk.reinbach@gmail.com>
2018-02-21 22:59:48 +0100

    Work in progress

--- main.cpp
+++ main.cpp
@@ -2,94 +2,83 @@
 #include "catch.hpp"
 
 #include <algorithm>
+#include <functional>
 #include <numeric>
+#include <optional>
 #include <string>
 #include <vector>
 
-//unsigned int Factorial( unsigned int number ) {
-//    return number <= 1 ? number : Factorial(number-1)*number;
-//}
-//
-//template <class T>
-//using Rule = std::function<std::optional<std::string> (T)>;
-//
-//template <class T>
-//auto operator + (Rule<T> const& lhs, Rule<T> const& rhs)
-//{
-//    return [=] (T value) {
-//        auto const l1 = lhs (value);
-//        auto const l2 = rhs (value);
-//
-//        if (! l1)
-//          return l2;
-//
-//        if (! l2)
-//          return l1;
-//
-//        return std::make_optional (*l1 + *l2);
-//    };
-//}
-//
-//template <class T>
-//auto operator || (Rule<T> const& lhs, Rule<T> const& rhs)
-//{
-//    return [=](T value) {
-//        auto const ret = lhs(value);
-//        return ret ? ret : rhs(value);
-//    };
-//}
-//
-//template <class T>
-//auto fizz (T value)
-//{
-//    if (value % 3)
-//      return std::make_optional ();
-//
-//}
+using namespace std::string_literals;
 
-auto fizzBuzz(int number)
-{
-  using namespace std::string_literals;
-  using std::to_string;
+using OptionalText = std::optional<std::string>;
 
-  if ((number % 3) == 0 && (number % 5) == 0)
-    return "FizzBuzz"s;
-
-  if ((number % 3) == 0)
-    return "Fizz"s;
-
-  if ((number % 5) == 0)
-    return "Buzz"s;
-
-  return to_string(number);
-}
-
-auto ints(int first, size_t count)
-{
-  auto v = std::vector<int>( count );
-  std::iota(begin(v), end(v), first);
-
-  return v;
-}
-
-auto ints(size_t count)
-{
-  return ints(0, count);
-}
+template <class T>
+using Rule = std::function<std::string(std::string, T)>;
 
 auto is_multiple_of(int factor)
 {
   return [=](auto value) { return (value % factor) == 0; };
 }
 
-using namespace std::string_literals;
-
-auto const is_multiple_of_three = is_multiple_of(3);
-auto const is_multiple_of_five = is_multiple_of(5);
-
-auto contains(std::string const& text, std::string pattern)
+auto multiple(int factor, std::string text)
 {
-  return text.find(pattern) != std::string::npos;
+  return Rule<int> {[=](auto output, auto value) {
+    auto mapping = std::map<bool, std::string>{ { true, text } };
+    return output + mapping[is_multiple_of(factor)(value)];
+  }};
+}
+
+auto fizz()
+{
+  return multiple(3, "Fizz"s);
+}
+
+auto buzz()
+{
+  return multiple(5, "Buzz"s);
+}
+
+template <class T>
+auto operator + (Rule<T> const& lhs, Rule<T> const& rhs)
+{
+  return Rule<T> {[=](auto output, auto value) {
+    return rhs(lhs(output, value), value);
+  }};
+}
+
+template <class T>
+auto compile(Rule<T> rule)
+{
+  return [=](auto value) {
+    return rule(""s, value);
+  };
+}
+
+auto fizzBuzz()
+{
+  return compile(fizz() + buzz());
+}
+
+template <class T>
+auto fizzBuzz(T value)
+{
+  return fizzBuzz()(value);
+}
+
+namespace range
+{
+  auto ints(int first, size_t count)
+  {
+    auto v = std::vector<int>(count);
+    std::iota(begin(v), end(v), first);
+
+    return v;
+  }
+
+  auto ints(size_t count)
+  {
+    return ints(0, count);
+  }
 }
 
 auto count(std::string const& text, std::string pattern, size_t pos = 0)
@@ -101,16 +90,22 @@ auto count(std::string const& text, std::string pattern, size_t pos = 0)
   return 1 + count(text, pattern, matchingPos + 1);
 }
 
-TEST_CASE( "Fizz is equivalent to a multiple of 3" ) {
+auto oneMatch(std::string const& text, std::string pattern)
+{
+  return count(text, pattern) == 1;
+}
 
-  for (auto const value : ints(100))
-    CHECK(contains (fizzBuzz(value), "Fizz"s) == is_multiple_of_three(value));
+TEST_CASE("Fizz is equivalent to a multiple of 3") {
+
+  // ints() | multiplyWith(3) | delimit (99)
+  for (auto const value : range::ints(100))
+    CHECK(oneMatch(fizzBuzz(value), "Fizz"s) == is_multiple_of(3)(value));
 }
 
 TEST_CASE("Buzz is equivalent to a multiple of 5") {
 
-  for (auto const value : ints(100))
-    CHECK(contains(fizzBuzz(value), "Buzz"s) == is_multiple_of_five(value));
+  for (auto const value : range::ints(100))
+    CHECK(oneMatch(fizzBuzz(value), "Buzz"s) == is_multiple_of(5)(value));
 }
 
 TEST_CASE("Fizz appears before Buzz") {
