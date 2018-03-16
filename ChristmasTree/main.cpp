ba20bb04aaa5738aa1815e66cfe64e5d4a366ddc
Dirk Reinbach <reinbach@volumegraphics.com>
2018-03-16 14:20:03 +0100

    Work in progress

--- ChristmasTree/main.cpp
+++ ChristmasTree/main.cpp
@@ -17,25 +17,10 @@ namespace christmas_tree
     return a;
   }
 
-  //using Transform = std::function<std::string(std::string)>;
-
-  //auto operator | (Image image, Transform transform)
-  //{
-  //  for (auto& line : image)
-  //    line = transform(line);
-
-  //  return image;
-  //}
-
-  //auto indent(std::string s)
-  //{
-  //  return Transform{ [=](auto line) { return s + line; } };
-  //}
-
   auto indent(Image image, std::string s)
   {
     for (auto& line : image)
-      line = s + line + s;
+      line = s + line;
 
     return image;
   }
@@ -45,17 +30,24 @@ namespace christmas_tree
     return image.back();
   }
 
-  using Images = std::map<int, Image>;
+  using ImageCatalog = std::map<int, Image>;
 
-  auto operator <= (std::string line, int heigth)
+  template <size_t... Is, class Tuple>
+  auto image_catalog_impl(std::index_sequence<Is...>, Tuple tuple) {
+    return ImageCatalog{
+      std::make_pair(std::get<2 * Is>(tuple), std::get<2 * Is + 1>(tuple))...
+    };
+  }
+  template <class... Args>
+  auto image_catalog(Args... args)
   {
-    return std::pair<int, Image> { heigth, Image{line} };
+    return image_catalog_impl (std::make_index_sequence<sizeof...(Args) / 2>{}, std::make_tuple (args...));
   }
 
   auto treetop(int height)
   {
     if (height <= 1)
-      return Images{ "X"s <= 1 }[height];
+      return image_catalog(1, Image{ "X"s })[height];
 
     auto const tree = treetop(height - 1);
     return indent (tree, " "s) + Image { "X"s + bottom (tree) + "X"s };
@@ -64,7 +56,7 @@ namespace christmas_tree
   auto trunk(int height)
   {
     if (height <= 1)
-      return Images{ "I"s <= 1 }[height];
+      return image_catalog(1, Image{ "I"s })[height];
 
     return indent (trunk(height - 1), " "s);
   }
